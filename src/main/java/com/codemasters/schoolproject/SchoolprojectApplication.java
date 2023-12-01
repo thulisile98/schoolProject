236e67b9be80e948e80540acaaa8cd0ff08d08be
Angel Mashego <thulisile.ma98@gmail.com>
2023-12-01 10:39:48 +0200

    added packages

---
+++ src/main/java/com/codemasters/schoolproject/SchoolprojectApplication.java
@@ -0,0 +1,13 @@
+package com.codemasters.schoolproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolprojectApplication.class, args);
+	}
+
+}
