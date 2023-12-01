462a5d8a3d5a5a02ef933597936f8b77329e0e1f
Angel Mashego <thulisile.ma98@gmail.com>
2023-12-01 10:39:48 +0200

    initial commit

---
+++ src/test/java/com/codemasters/schoolproject/SchoolprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemasters.schoolproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
